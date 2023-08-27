b57977e1db11c784758f8c37435789d44d78a3ca
Abu Raihan <raihanahmed01973@gmail.com>
2023-08-27 11:45:37 +0600

    added per page banner component

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,8 +1,12 @@
 import React from "react";
+import PageBanner from "../../components/PageBanner/PageBanner";
 
 const About = () => {
+  const title = "About Us";
+  const description = "This is our About page, here you can know more about us";
   return (
     <div>
+      <PageBanner title={title} description={description} />
       <div className="text-2xl">about</div>
     </div>
   );
---
+++ src/components/PageBanner/PageBanner.js
@@ -0,0 +1,22 @@
+import React from "react";
+import img from "../../../src/assets/images/group.jpg";
+const PageBanner = ({ title, description }) => {
+  return (
+    <div className="lg:h-[450px] h-[350px] relative">
+      <div className="relative">
+        <img src={img} alt="" className="w-full lg:h-[450px] h-[350px]" />
+        <div className="lg:top-20 lg:left-20 left-10 top-10 absolute z-10">
+          <h2 className="  lg:text-5xl text-3xl font-bold z-10 text-primary">
+            {title}
+          </h2>
+          <p className="text-white mt-4 font-semibold lg:w-3/4 pr-5  text-justify text-lg">
+            {description}
+          </p>
+        </div>
+      </div>
+      <div className="absolute inset-0 bg-black opacity-70"></div>
+    </div>
+  );
+};
+
+export default PageBanner;
