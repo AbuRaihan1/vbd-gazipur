3b9ac53a73be71210676d5958101be56bbc5380f
Abu Raihan <raihanahmed01973@gmail.com>
2023-08-29 00:28:48 +0600

    added projects detals page and working fine

--- src/Pages/projects/ProjectDetails.js
+++ src/Pages/projects/ProjectDetails.js
@@ -10,12 +10,26 @@ const ProjectDetails = () => {
         setItem(items);
       })
     );
-  }, []);
-  
+  }, [params.id]);
+
+  const { title, description, img } = item;
+  console.log(img);
   return (
-    <div>
-      {/* <div>{title}</div> */} <p>details</p>
-      <p> titel : {item.title}</p>
+    <div className="container mx-auto shadow-2xl rounded-xl p-5 my-20">
+      <div className="grid lg:grid-cols-2 gap-6">
+        <div>
+          {" "}
+          <img src={img} alt="img" className="rounded-2xl" />
+        </div>
+        <div>
+          <h2 className="text-secondary font-bold lg:text-5xl text-3xl text-center">
+            {title}
+          </h2>
+          <p className="font-semibold mt-5 text-lg text-justify">
+            {description}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/PageBanner/PageBanner.js
+++ src/components/PageBanner/PageBanner.js
@@ -2,9 +2,9 @@ import React from "react";
 import img from "../../../src/assets/images/group.jpg";
 const PageBanner = ({ title, description }) => {
   return (
-    <div className="lg:h-[450px] h-[350px] relative">
+    <div className="lg:h-[500px] h-[350px] relative">
       <div className="relative">
-        <img src={img} alt="" className="w-full lg:h-[450px] h-[350px]" />
+        <img src={img} alt="" className="w-full lg:h-[500px] h-[350px]" />
         <div className="lg:top-20 lg:left-20 left-10 top-10 absolute z-10">
           <h2 className="  lg:text-5xl text-3xl font-bold z-10 text-primary">
             {title}
