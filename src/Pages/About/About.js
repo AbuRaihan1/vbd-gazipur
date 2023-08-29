a7bc3764a9e6c4d95600b41cd078803e23eab763
Abu Raihan <raihanahmed01973@gmail.com>
2023-08-29 12:49:18 +0600

    added mission and vission

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,15 +2,18 @@ import React from "react";
 import PageBanner from "../../components/PageBanner/PageBanner";
 import AboutCompo from "../../components/AboutCompo/AboutCompo";
 import Statistics from "./Statistics";
+import Mission from "./Mission";
 
 const About = () => {
   const title = "About Us";
-  const description = "This is our About page, here you can know more about us";
+  const description =
+    "Volunteer for Bangladesh is a youth wing of JAAGO Foundation, established in 2011 with support of U.S Embassy to Bangladesh. Its goal is to empower young people in Bangladesh to volunteer and develop leadership skills to make positive change in their communities. It provides platform for community service, volunteer work, training, and mentorship opportunities";
   return (
     <div>
       <PageBanner title={title} description={description} />
       <AboutCompo />
       <Statistics />
+      <Mission />
     </div>
   );
 };
