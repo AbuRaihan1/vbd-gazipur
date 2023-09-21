beedcb07ae6cf9b76d838ddd447e9070370ddfc3
Abu Raihan <raihanahmed01973@gmail.com>
2023-09-21 22:59:42 +0600

    added loader until loaded projects

--- src/Pages/AllProjects/AllProjects.js
+++ src/Pages/AllProjects/AllProjects.js
@@ -5,12 +5,18 @@ import { GrPrevious, GrNext } from "react-icons/gr";
 import projectBanner from "../../../src/assets/images/project_Banner.jpg";
 import AOS from "aos";
 import "aos/dist/aos.css";
+
+const Loader = () => {
+  return <div className="loader">Projects Loading...</div>;
+};
+
 const AllProjects = () => {
   useEffect(() => {
     AOS.init({
       duration: 2000,
     });
   }, []);
+
   const title = "Our Projects";
   const description =
     "Since creating our teams, we have completed numerous projects, and we are currently working on a new project. Day by day, we are contemplating how we can make a positive impact on our society through our projects.";
@@ -19,6 +25,7 @@ const AllProjects = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
   const itemsPerPage = 6;
+  const [isLoading, setIsLoading] = useState(true); // Initialize loading state
 
   useEffect(() => {
     fetch("/featuresProjectsData.json")
@@ -28,10 +35,10 @@ const AllProjects = () => {
         const startIndex = (currentPage - 1) * itemsPerPage;
         const endIndex = startIndex + itemsPerPage;
         setAllProjects(data.slice(startIndex, endIndex));
+        setIsLoading(false); // Set loading state to false when data is loaded
       });
   }, [currentPage]);
 
-  console.log(allProjects);
   return (
     <div className="mb-16">
       <PageBanner title={title} img={projectBanner} description={description} />
@@ -39,11 +46,16 @@ const AllProjects = () => {
       <h2 className="lg:text-5xl text-3xl font-bold text-secondary text-center my-16">
         All Projects
       </h2>
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 mt-10 mb-20 container mx-auto">
-        {allProjects.map((project, idx) => (
-          <AllProjectsCard project={project} key={idx} />
-        ))}
-      </div>
+
+      {isLoading ? (
+        <Loader /> // Show the loader while loading
+      ) : (
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 mt-10 mb-20 container mx-auto">
+          {allProjects.map((project, idx) => (
+            <AllProjectsCard project={project} key={idx} />
+          ))}
+        </div>
+      )}
 
       {allProjects ? (
         <div className="pagination text-center flex items-center justify-center gap-5">
@@ -78,7 +90,7 @@ const AllProjects = () => {
             className={`${
               currentPage === totalPages
                 ? "opacity-50 cursor-not-allowed "
-                : " hover:bg-secondary duration-150 bg-[#dad4d442]"
+                : " hover-bg-secondary duration-150 bg-[#dad4d442]"
             } px-4 py-2 border rounded-full h-12 text-white w-12`}
           >
             <GrNext />
