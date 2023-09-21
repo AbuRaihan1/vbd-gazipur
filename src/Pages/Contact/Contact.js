e9b73663ca221eae1be2b58baecfae975ef1e9d2
Abu Raihan <raihanahmed01973@gmail.com>
2023-09-21 22:06:31 +0600

    added contact us page banner photo

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,14 +1,23 @@
 import React from "react";
 import PageBanner from "../../components/PageBanner/PageBanner";
-
+import contactIMG from "../../assets/images/contact.svg";
 const Contact = () => {
   const title = "Contact Us";
   const description =
     "This is our contact page, you can know about our contact and any other information from here";
   return (
     <div>
-      <PageBanner title={title} description={description} />
-      <div className="text-2xl">contact</div>
+      <PageBanner img={contactIMG} title={title} description={description} />
+      <div className="my-10 text-center">
+        <h2 className="lg:text-5xl text-secondary top-10 font-bold text-3xl">
+          Contact us
+        </h2>
+      </div>
+
+      <div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
